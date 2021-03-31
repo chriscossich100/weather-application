1d463db939219550e24c4f9493479d75ed8ab426
CHRISTOPHR COSSICH <chriscossich@gmail.com>
2021-03-30 20:25:04 -0400

    small changes were made

--- home.js
+++ home.js
@@ -38,7 +38,7 @@ $(function(){
 
     function getLocation(){
         var date = new Date();
-        const apiKey = "48ef76dd06c684c6abd02d37d95a18b1";
+        const apiKey = "YOUR API KEY HERE";
         $.get(`https://api.openweathermap.org/data/2.5/weather?q=Leesburg&units=${unitOfMeasurement}&appid=${apiKey}`, function(data, status){
             console.log(data);
 
